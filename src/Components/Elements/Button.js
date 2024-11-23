6920f764ab0948d1bbe59a32f06f7e9a8c29ad31
Cliffhanger9 <129917396+Cliffhanger9@users.noreply.github.com>
2024-11-22 23:52:39 -0500

    Added Landlord Home
    Adjusted general.css to increase spacing between words.
    Made change to HeaderText to allow inline styles as I was trying to create the purple box and a headerText combined for reuse on further details page.

--- src/Components/Elements/Button.js
+++ src/Components/Elements/Button.js
@@ -1,8 +1,15 @@
 import "../../Styles/Button.css";
 const UserButton = ({ text, green, onClick }) => {
+  const handleClick = () => {
+    console.log(`Button clicked: ${text}`); // Log the button text or any desired information
+    if (onClick) {
+      onClick(); // Call the passed onClick function, if any
+    }
+  };
+
   return (
     <>
-      <button className={green ? "greenButton" : "redButton"} onClick={onClick}>
+      <button className={green ? "greenButton" : "redButton"} onClick={handleClick}>
         {text}
       </button>
     </>
---
+++ src/Components/Elements/PurpleBox.js
@@ -0,0 +1,23 @@
+import "../../Styles/purpleBox.css"
+import HeaderText from "./HeaderText";
+const PurpleBox = ({w1Text, bText, w2Text}) =>{
+    return( 
+    
+
+        
+    <div className = "PurpleBox-container">
+        <HeaderText 
+        large = {true}
+        w1Text={w1Text}
+        bText = {bText}
+        w2Text={w2Text}
+        style={{position: "relative", top: "150px"}}
+        
+        ></HeaderText>
+        <div className ="PurpleBox"></div>
+    </div>
+
+    );
+
+};
+export default PurpleBox;
